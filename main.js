b8825f02ab39150dfff531fa49d210d9bf293250
skoorupa <zaxad23@gmail.com>
2018-02-03 15:12:47 +0100

    ws

--- main.js
+++ main.js
@@ -5,16 +5,31 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename;
-  if (q.pathname) {
-    filename = "." + q.pathname;
-  } else {
-    filename = ".index.html";
-  }
+  console.log("."+q.pathname);
+
+  var deny = ["/main.js","/package.json","package-lock.json"];
+  var doyoudeny = false;
+
+  deny.forEach(function (item, index) {
+    if (item==q.pathname+"")
+      doyoudeny = true;
+  });
+
+  if (q.pathname!="/")
+    filename = "."+q.pathname;
+  else
+    filename = "./index.html";
 
   fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+    if (err || doyoudeny) {
+      if (doyoudeny){
+        console.log("connection denied");
+        res.writeHead(403, {'Content-Type': 'text/html'});
+        return res.end("403 Forbidden");
+      } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("404 Not Found :(");
+      }
     }
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
